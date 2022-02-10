42ced67619516057264dee150d7cef30aee638bd
yersonrincon <yersonhernandez202@gmail.com>
2022-02-10 17:38:26 -0500

    quito comit

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.use((req, res, next) => {
         "http://localhost:4201",
         "http://localhost:8444",
         "https://lapesada.herokuapp.com",
+        "https://lapesada.herokuapp.com/",
+        "https://git.heroku.com/lapesada.git"
        
     ];
 
