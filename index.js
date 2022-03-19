645a75be71e43347813e874f4e165c4f243da36e
yersonrincon <yersonhernandez@gmail.com>
2022-03-19 07:22:34 -0500

    make it better

--- index.js
+++ index.js
@@ -41,12 +41,9 @@ app.use((req, res, next) => {
         "http://localhost:4200",
         "http://localhost:4201",
         "http://localhost:8444",
-       "https://lapesada.herokuapp.com",
+        "https://lapesada.herokuapp.com",
         "https://lapesada.herokuapp.com/",
-       "https://git.heroku.com/lapesada.git"
-      // "https://lapesadaempresa.herokuapp.com/",
-     //  "https://git.heroku.com/lapesadaempresa.git"
-
+        "https://git.heroku.com/lapesada.git"
        
     ];
 
