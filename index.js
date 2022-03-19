e4066274aef1e6b3c4c4ed7aecfe955574b92f53
yersonrincon <yersonhernandez@gmail.com>
2022-03-19 00:32:58 -0500

    make it better

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ app.use((req, res, next) => {
         "http://localhost:4200",
         "http://localhost:4201",
         "http://localhost:8444",
-       // "https://lapesada.herokuapp.com",
-      //  "https://lapesada.herokuapp.com/",
-      //  "https://git.heroku.com/lapesada.git"
-      "https://lapesadaempresa.herokuapp.com/"
+        "https://lapesada.herokuapp.com",
+        "https://lapesada.herokuapp.com/",
+        "https://git.heroku.com/lapesada.git"
+       
     ];
 
     let origin = req.headers.origin;
