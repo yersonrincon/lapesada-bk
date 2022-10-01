826780462976844b7601726e5ce6db6dbeefd697
yerson <yersonhernandez202@gmail.com>
2022-09-30 22:13:20 -0500

    cors agregados

--- config/index.js
+++ config/index.js
@@ -8,20 +8,7 @@ const config = {
     authJwtSecret: process.env.AUTH_JWT_SECRET,
     expiresIn: process.env.EXPIRES_IN,
 }
-/*const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    //port: parseInt(process.env.DB_PORT, 10),
-    parseJSON: true,
-    connectionTimeout: 300000,
-    requestTimeout: 300000,
-    pool: {
-        idleTimeoutMillis: 300000,
-        max: 100
-    }
-};*/
+
 const dbConfig = {
     user: 'eeomlysybxiukm',
     host: 'ec2-34-192-83-52.compute-1.amazonaws.com',
