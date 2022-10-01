d914ee8925fcd118c4361501572d31a927abf9c3
yerson <yersonhernandez202@gmail.com>
2022-09-30 19:22:25 -0500

    cors agregados

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ app.use((req, res, next) => {
         "http://localhost:4201",
         "http://localhost:8444",
         "https://lapesada.herokuapp.com",
+        "http://lapesada.herokuapp.com",
+        "http://lapesada.herokuapp.com/",
         "https://lapesada.herokuapp.com/",
         "https://git.heroku.com/lapesada.git"
        
